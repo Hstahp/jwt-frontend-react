6cfbeb2b0ab890becec075b083da13ae0b78a8c2
Huy Phatt <phatle219@gmail.com>
2026-07-23 10:59:27 +0700

    feat: use Router and make layout for login with bootstrap5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import logo from './logo.svg';
 import './App.scss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Nav from './components/Navigation/Nav';
+import './components/Navigation/Nav.scss';
+import Login from './components/Login/Login';
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Hello world React with Hoi Dan IT</p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Router>
+            <div className="app-container">
+                {/* <Nav /> */}
+                <Switch>
+                    <Route path="/news">News</Route>
+                    <Route path="/home" excat>
+                        Home
+                    </Route>
+                    <Route path="/contact">Contact</Route>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/about">About</Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
