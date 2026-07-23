2afbb839ae672b11a54603a9aee1529ba383b8b1
Huy Phatt <phatle219@gmail.com>
2026-07-23 21:02:59 +0700

    feat: form login responsive

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,18 +2,25 @@ import './Login.scss';
 
 function Login() {
     return (
-        <div className="login-container mt-3">
+        <div className="login-container ">
             <div className="container">
-                <div className="row">
-                    <div className="content-left col-7">
+                <div className="row px-3 px-sm-0">
+                    <div className="content-left col-sm-7 col-12 d-none d-sm-block gap-3 py-3 ">
                         <div className="brand">Facebook</div>
-                        <div className="detail">Facebook helps you connect and share with the people in your life</div>
+                        <div className="detail text-sm-start text-md-center">
+                            Facebook helps you connect and share with the people in your life
+                        </div>
                     </div>
-                    <div className="content-right col-5 d-flex flex-column gap-3 py-3">
+                    <div className="content-right col-12 col-sm-5 d-flex flex-column gap-3 py-3 ">
+                        <div className="brand d-sm-none ">Facebook</div>
                         <input type="text" className="form-control" placeholder="Email address or phone number" />
                         <input type="password" className="form-control" placeholder="Password" />
                         <button className="btn btn-primary">Login</button>
-                        <span className="text-center">Forgot your password?</span>
+                        <span className="text-center">
+                            <a className="forgot-password" href="#">
+                                Forgot your password?
+                            </a>
+                        </span>
                         <hr />
                         <div className="text-center">
                             <button className="btn btn-success">Create new account</button>
