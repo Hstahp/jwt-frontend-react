e9231f303f2b9647a845685d1e0afde8521f399e
Huy Phatt <phatle219@gmail.com>
2026-07-25 21:04:44 +0700

    feat: test api login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,46 @@
 import './Login.scss';
 import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+
+import { loginUser } from '../../services/userService';
+
 function Login() {
     const history = useHistory();
+
+    const [valueLogin, setValueLogin] = useState('');
+    const [password, setPassword] = useState('');
+
+    const defaultObjValidInput = {
+        isValidValueLogin: true,
+        isValidPassword: true,
+    };
+    const [objInputs, setObjInputs] = useState(defaultObjValidInput);
 
     const handleCreateNewAccount = () => {
         history.push('/register');
     };
+
+    const handleLogin = async () => {
+        setObjInputs(defaultObjValidInput);
+
+        if (!valueLogin) {
+            setObjInputs({ ...objInputs, isValidValueLogin: false });
+            toast.error('Please enter your email address or your phone number');
+            return;
+        }
+        if (!password) {
+            setObjInputs({ ...objInputs, isValidPassword: false });
+            toast.error('Please enter your password');
+            return;
+        }
+        if (valueLogin && password) {
+            toast.success('Login successfully');
+        }
+
+        await loginUser(valueLogin, password);
+    };
+
     return (
         <div className="login-container ">
             <div className="container">
@@ -19,14 +54,33 @@ function Login() {
                     <div className="content-right col-12 col-sm-5 d-flex flex-column gap-3 py-3 ">
                         <div className="brand d-sm-none ">Facebook</div>
                         <div className="form-group form-floating">
-                            <input type="text" className="form-control" placeholder="Email address or phone number" />
+                            <input
+                                type="text"
+                                className={objInputs.isValidValueLogin ? 'form-control' : 'form-control is-invalid'}
+                                value={valueLogin}
+                                onChange={(event) => setValueLogin(event.target.value)}
+                                placeholder="Email address or phone number"
+                            />
                             <label>Email address or phone number</label>
                         </div>
                         <div className="form-group form-floating">
-                            <input type="password" className="form-control" placeholder="Password" />
+                            <input
+                                type="password"
+                                className={objInputs.isValidPassword ? 'form-control' : 'form-control is-invalid'}
+                                value={password}
+                                onChange={(event) => setPassword(event.target.value)}
+                                placeholder="Password"
+                            />
                             <label>Password</label>
                         </div>
-                        <button className="btn btn-primary">Login</button>
+                        <button
+                            className="btn btn-primary"
+                            onClick={() => {
+                                handleLogin();
+                            }}
+                        >
+                            Login
+                        </button>
                         <span className="text-center">
                             {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                             <a className="forgot-password" href="#">
--- src/services/userService.js
+++ src/services/userService.js
@@ -9,4 +9,8 @@ const registerNewUser = (email, phone, password, username) => {
     });
 };
 
-export { registerNewUser };
+const loginUser = (valueLogin, password) => {
+    return axios.post('http://localhost:8080/api/v1/login', { valueLogin, password });
+};
+
+export { registerNewUser, loginUser };
