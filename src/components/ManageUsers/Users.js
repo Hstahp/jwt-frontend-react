99e3150c3e6d0398bfaa9053549e84ae72342637
Huy Phatt <phatle219@gmail.com>
2026-07-28 16:21:49 +0700

    feat: delete user

--- src/components/ManageUsers/Users.js
+++ src/components/ManageUsers/Users.js
@@ -1,104 +1,148 @@
 import { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import { toast } from 'react-toastify';
 
 import './Users.scss';
-import { fetchAllUser } from '../../services/userService';
+import { deleteUser, fetchAllUser } from '../../services/userService';
+import ModalDelete from './ModalDelete';
+
 function Users() {
     const [listUsers, setListUsers] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [currentLimit, setCurrentLimit] = useState(3);
+    const [totalPages, setTotalPages] = useState(0);
+    const [isShowModalDelete, setIsShoModalDelete] = useState(false);
+    const [dataModal, setDataModal] = useState({});
+
     useEffect(() => {
         fetchUsers();
-    }, []);
+    }, [currentPage]);
 
     const fetchUsers = async () => {
-        let response = await fetchAllUser();
+        let response = await fetchAllUser(currentPage, currentLimit);
         if (response && response.data && response.data.EC === 0) {
-            setListUsers(response.data.DT);
+            setTotalPages(response.data.DT.totalPages);
+            setListUsers(response.data.DT.users);
         }
     };
 
+    const handlePageClick = async (event) => {
+        setCurrentPage(+event.selected + 1);
+        await fetchUsers();
+    };
+
+    const handleDeleteUser = async (user) => {
+        setDataModal(user);
+        setIsShoModalDelete(true);
+    };
+
+    const handleClose = () => {
+        setIsShoModalDelete(false);
+        setDataModal({});
+    };
+
+    const confirmedDeleteUser = async () => {
+        let response = await deleteUser(dataModal);
+        if (response && response.data.EC === 0) {
+            toast.success(response.data.EM);
+            await fetchUsers();
+            setIsShoModalDelete(false);
+        } else {
+            toast.error(response.data.EM);
+        }
+    };
     return (
-        <div className="container">
-            <div className="manage-users-container">
-                <div className="user-header">
-                    <div>
-                        <h3></h3>
-                        <div className="actions">
-                            <button className="btn btn-success">Refesh</button>
-                            <button className="btn btn-primary">Add new user</button>
+        <>
+            <div className="container">
+                <div className="manage-users-container">
+                    <div className="user-header">
+                        <div>
+                            <h3>Table User</h3>
+                            <div className="actions">
+                                <button className="btn btn-success">Refesh</button>
+                                <button className="btn btn-primary">Add new user</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="user-body">
-                    <table className="table table-hover table-bordered">
-                        <thead>
-                            <tr>
-                                <th scope="col">No</th>
-                                <th scope="col">Id</th>
-                                <th scope="col">Email</th>
-                                <th scope="col">Username</th>
-                                <th scope="col">Group</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {listUsers && listUsers.length > 0 ? (
-                                <>
-                                    {listUsers.map((item, index) => {
-                                        return (
-                                            <tr key={`row-${index}`}>
-                                                <td>{index + 1}</td>
-                                                <td>{item.id}</td>
-                                                <td>{item.email}</td>
-                                                <td>{item.username}</td>
-                                                <td>{item.Group ? item.Group.name : ''}</td>
-                                            </tr>
-                                        );
-                                    })}
-                                </>
-                            ) : (
-                                <>
-                                    <span>Not found users</span>
-                                </>
-                            )}
-                        </tbody>
-                    </table>
-                </div>
-                <div className="user-footer">
-                    <nav aria-label="Page navigation example">
-                        <ul className="pagination">
-                            <li className="page-item">
-                                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                                <a className="page-link" href="#">
-                                    Previous
-                                </a>
-                            </li>
-                            <li className="page-item">
-                                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                                <a className="page-link" href="#">
-                                    1
-                                </a>
-                            </li>
-                            <li className="page-item">
-                                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                                <a className="page-link" href="#">
-                                    2
-                                </a>
-                            </li>
-                            <li className="page-item">
-                                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                                <a className="page-link" href="#">
-                                    3
-                                </a>
-                            </li>
-                            <li className="page-item">
-                                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                                <a className="page-link" href="#">
-                                    Next
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
+                    <div className="user-body">
+                        <table className="table table-hover table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col">No</th>
+                                    <th scope="col">Id</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Username</th>
+                                    <th scope="col">Group</th>
+                                    <th scope="col"> Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {listUsers && listUsers.length > 0 ? (
+                                    <>
+                                        {listUsers.map((item, index) => {
+                                            return (
+                                                <tr key={`row-${index}`}>
+                                                    <td>{index + 1}</td>
+                                                    <td>{item.id}</td>
+                                                    <td>{item.email}</td>
+                                                    <td>{item.username}</td>
+                                                    <td>{item.Group ? item.Group.name : ''}</td>
+                                                    <td>
+                                                        <button className="btn btn-warning mx-1">Edit</button>
+                                                        <button
+                                                            className="btn btn-danger"
+                                                            onClick={() => handleDeleteUser(item)}
+                                                        >
+                                                            Delete
+                                                        </button>
+                                                    </td>
+                                                </tr>
+                                            );
+                                        })}
+                                    </>
+                                ) : (
+                                    <tr>
+                                        <td colSpan={6} className="text-center">
+                                            Not found users
+                                        </td>
+                                    </tr>
+                                )}
+                            </tbody>
+                        </table>
+                    </div>
+                    {totalPages > 0 && (
+                        <div className="user-footer">
+                            <ReactPaginate
+                                nextLabel="next >"
+                                onPageChange={handlePageClick}
+                                pageRangeDisplayed={3}
+                                marginPagesDisplayed={2}
+                                pageCount={totalPages}
+                                previousLabel="< previous"
+                                pageClassName="page-item"
+                                pageLinkClassName="page-link"
+                                previousClassName="page-item"
+                                previousLinkClassName="page-link"
+                                nextClassName="page-item"
+                                nextLinkClassName="page-link"
+                                breakLabel="..."
+                                breakClassName="page-item"
+                                breakLinkClassName="page-link"
+                                containerClassName="pagination"
+                                activeClassName="active"
+                                renderOnZeroPageCount={null}
+                            />
+                        </div>
+                    )}
                 </div>
             </div>
-        </div>
+            <ModalDelete
+                show={isShowModalDelete}
+                handleClose={handleClose}
+                confirmedDeleteUser={confirmedDeleteUser}
+                dataModal={dataModal}
+            />
+        </>
     );
 }
 
